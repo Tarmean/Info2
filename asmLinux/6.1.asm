7d62960cbf252870a688b330db85cc300d159097
cyfah100@hhu.de <Tarmean>
2015-05-25 17:44:38 +0200

    fixes

--- asmLinux/6.1.asm
+++ asmLinux/6.1.asm
@@ -8,6 +8,8 @@
 segment .data
 prompt1 db    "Enter the Adress: ", 0
 prompt2 db    "The row is: ", 0
+prompt3 db    "The tag is: ", 0
+error   db    "The Adress has to be between 0 and 65535", 0
 
 
 
@@ -26,15 +28,29 @@ asm_main:
         call print_string
 
         call read_int                                                   ; read integer to eax
-        mov ebx, eax
 
-        shr ebx, 4                                                      ; move 4 bits to the right
+        test eax, 11111111111111110000000000000000b                     ;is anything but the first 16 bit set? Dirty tricks because I am too lazy to find the right jump instruction for unsigned compares
+        jnz abort
+
+                                                                        ;  I hope ccccccccrrraawwb is the right thingy 
+        shr eax, 5                                                      ; move 4 bits to the right. leaving ccccccccrrr 
+        mov ebx, eax
+        and ebx, 00000000000000000000000000000111b                      ; mask the rest out, leaving rrr
+
+        shr eax, 3                                                      ; move 3 bits to the right. Leaving cccccccc 
+        mov ecx, eax
 
         mov eax, prompt2                                                ; print prompt and result
-        call print_string
+        call print_string                                                   
         mov eax, ebx
         call print_int
+        call print_nl
 
+        mov eax, prompt3                                                ; print prompt and result
+        call print_string                                                   
+        mov eax, ecx
+        call print_int
+        call print_nl
 
 
         popa
@@ -42,4 +58,12 @@ asm_main:
         leave
         ret
 
+abort:
 
+        mov eax, error                                                ; print prompt and result
+        call print_string                                                   
+        call print_nl
+        popa
+        mov     eax, 0                                                  ; return back to C
+        leave
+        ret
